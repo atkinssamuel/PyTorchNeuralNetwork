222da7a0d108feddbc1447c544f6123a3a11cf24
atkinssamuel <purpleandgold73@gmail.com>
2019-12-01 22:22:50 -0500

    19-12-1 Created base model, training functions, and accuracy functions. The model is failing to overfit to the training dataset. Will investigate with another dataset.

--- data/data_parser.py
+++ data/data_parser.py
@@ -1,16 +1,38 @@
 import csv
 import numpy as np
+import torch
+
+
+class Dataset():
+    def __init__(self, inputs, labels):
+        self.labels = labels
+        self.inputs = inputs
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, index):
+        x = self.inputs[index]
+        y = self.labels[index]
+
+        return x, y
+
 
 def get_dataset(file_name):
-    with open(file_name) as csv_file:
+    with open('../data/' + file_name) as csv_file:
         csv_reader = csv.reader(csv_file)
         init = 0
         for row in csv_reader:
-            row_element = np.array(row).reshape(1, 9)
+            input_element = np.array(row[:8], dtype=float).reshape(1, 8)
+            output_element = np.array(row[-1], dtype=float)
             if init == 0:
-                dataset = row_element
+                inputs = input_element
+                outputs = output_element
                 init = 1
             else:
-                dataset = np.vstack((dataset, row_element))
-    return dataset
+                inputs = np.vstack((inputs, input_element))
+                outputs = np.vstack((outputs, output_element))
+    inputs = torch.tensor(inputs)
+    outputs = torch.tensor(outputs)
+    return Dataset(inputs, outputs)
 
--- train/train.py
+++ train/train.py
@@ -1,10 +1,105 @@
 from data.data_parser import get_dataset
-from model.model_architecture import FullyConnected
+from architecture.model_architecture import FullyConnected
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
 
-def train(learning_rate, batch_size):
 
+def get_accuracy(model, data):
+    correct = 0
+    total = 0
+    data_loader = torch.utils.data.DataLoader(data, batch_size=data.__len__(), shuffle=False)
+    for inputs, labels in data_loader:
+        outputs = model(inputs.float()).detach().numpy()
+        outputs = np.round(outputs)
+        labels = labels.detach().numpy()
+        total += inputs.shape[0]
+        wrong = np.sum(np.abs(outputs - labels))
+        correct += inputs.shape[0] - wrong
+    return correct/total
+
+
+def get_loss(model, data):
+    data_loader = torch.utils.data.DataLoader(data, batch_size=data.__len__(), shuffle=False)
+    criterion = nn.CrossEntropyLoss()
+    for inputs, labels in iter(data_loader):
+        outputs = model(inputs.float())
+        loss = criterion(outputs, labels)
+    return float(loss) / data.__len__()
+
+
+def train(model, name, training_data, validation_data=None, batch_size=1, epoch_count=1, shuffle=False,
+          learning_rate=0.01, checkpoint_frequency=5, momentum=0.9):
+    train_loader = torch.utils.data.DataLoader(training_data, batch_size=batch_size, shuffle=shuffle)
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
+    iterations, losses, train_acc, validation_acc, validation_loss = [], [], [], [], []
+
+    # training
+    current_iteration = 0  # the number of iterations
+
+    for epoch in range(epoch_count):
+        for inputs, labels in iter(train_loader):
+            outputs = model(inputs.float())  # forward pass
+            loss = criterion(outputs, labels)  # compute the total loss
+            loss.backward()  # backward pass (compute parameter updates)
+            optimizer.step()  # make the updates for each parameter
+            optimizer.zero_grad()  # a clean up step for PyTorch
+
+            # save the current training information
+            iterations.append(current_iteration)
+            losses.append(float(loss) / batch_size)  # compute *average* loss
+            train_acc.append(get_accuracy(model, training_data))  # compute training accuracy
+
+            if validation_data is not None:
+                validation_acc.append(get_accuracy(model, validation_data))
+                validation_loss.append(get_loss(model, validation_data))
+
+            # checkpoint:
+            if current_iteration % checkpoint_frequency == 0:
+                print("Current Training Accuracy at Iteration {}: {}".format(current_iteration, train_acc[-1]))
+
+                model_path = '../models/' + str(name) + '_' + str(current_iteration) + '_' + str(batch_size) + \
+                             '_' + str(learning_rate)
+                torch.save(model.state_dict(), model_path)
+
+            current_iteration += 1
+
+    # plotting
+    plt.title("Training Curve")
+    plt.plot(iterations, losses, label="Train")
+    plt.xlabel("Iterations")
+    plt.ylabel("Loss")
+    plt.savefig("../results/training_loss.png")
+    plt.title("Training Curve")
+    plt.plot(iterations, train_acc, label="Train")
+    plt.xlabel("Iterations")
+    plt.ylabel("Training Accuracy")
+    plt.legend(loc='best')
+    plt.savefig("../results/training_accuracy.png")
+    if validation_data is not None:
+        plt.title("Validation Accuracy")
+        plt.plot(validation_acc, label="Validation")
+        plt.xlabel("Iterations")
+        plt.ylabel("Validation Accuracy")
+        plt.legend(loc='best')
+        plt.savefig("../results/validation_accuracy.png")
+        plt.title("Validation Loss")
+        plt.plot(validation_loss, label="Validation")
+        plt.xlabel("Iterations")
+        plt.ylabel("Validation Loss")
+        plt.legend(loc='best')
+        plt.savefig("../results/validation_loss.png")
+
+    print("Final Training Accuracy: {}".format(train_acc[-1]))
+
+
+##
 
 if __name__ == "__main__":
     file_name = 'pima-indians-diabetes.data.csv'
-    dataset = get_dataset(file_name)
-
+    dataset_object = get_dataset(file_name)
+    basicFC = FullyConnected()
+    train(basicFC, "basicFC", dataset_object, batch_size=128, epoch_count=20, shuffle=True, learning_rate=0.01)
